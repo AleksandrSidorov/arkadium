272569a021d68f24ffbbd8971ac9e5357e15b4ea
Aleksandr Sidorov <shtil80@gmail.com>
2017-04-18 17:18:57 +0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Menu from './Menu'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, World</h1>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+	render () {
+		return (
+			<div>
+				<h2>This is menu</h2>
+				<ul>
+					<li>Today</li>
+					<li>This week</li>
+					<li>All time</li>
+				</ul>
+			</div>
+		)
+	}
+}
+
+export default Menu;
